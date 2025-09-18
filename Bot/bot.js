833af5bf0a31efe5688adf95b9d0625d83aba6ad
Viktree234 <135076900+Viktree234@users.noreply.github.com>
2025-09-18 21:07:51 +0000

    new

--- Bot/bot.js
+++ Bot/bot.js
@@ -60,7 +60,7 @@ async function connectBot() {
 
         if (text) {
             try {
-                const res = await fetch("http://127.0.0.1:5000/chat", {
+                const res = await fetch("https://baby-gpt.onrender.com/chat", {
                     method: "POST",
                     headers: { "Content-Type": "application/json" },
                     body: JSON.stringify({ message: text }),
